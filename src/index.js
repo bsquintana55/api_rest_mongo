bb20b48139d30db8994b041689537c059cc8b0a3
GabrielaMoron <104436274+GabrielaMoron@users.noreply.github.com>
2022-11-27 20:36:16 -0500

    conexion

--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const url_mongo = process.env.DISQ_URL;
-//console.log(url_mongo)
 
-mongoose.connect(url_mongo);
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('base de datos disq conectada');
-})
 
 app.use('/cancion', routes)
 
 
 
+try {
+    mongoose.connect(
+        "mongodb://localhost:27017/Disquera",
+        {
+            //opciones de seguridad 
+            useNewUrlParser: true,
+            //devolvera de una manera especifica las respuestas que se encuentran ahí
+            useUnifiedTopology: true
+        }
+    )
+        .then(() => console.log("conecccion de DB realizada!"));
+} catch (error) {
+    console.log(error);
+}
 
 const app = express();
 
