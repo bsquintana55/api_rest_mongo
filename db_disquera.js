18c56c07f29f69b96cc3245d84ca55f0a5518239
bsquintana55 <bsquintana55@misena.edu.co>
2022-12-05 16:09:10 -0500

    05 de dic - rutas

--- db_disquera.js
+++ db_disquera.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const conexionBD=async()=>{
    // const uri=`mongodb+srv://solq44:<password>@cluster0.hx2oziu.mongodb.net/?retryWrites=true&w=majority`;
     try{
-        const DB=await mongoose.connect('mongodb+srv://solq44:solq44@cluster0.hx2oziu.mongodb.net/?retryWrites=true&w=majority');
+        const DB=await mongoose.connect('mongodb+srv://solq44:sol44q@cluster0.hx2oziu.mongodb.net/?retryWrites=true&w=majority');
         //mongodb+srv://solq44:sol44q@cluster0.hx2oziu.mongodb.net/disquera
         console.log("Conexión satisfactoria",DB.connection.name);
     }
